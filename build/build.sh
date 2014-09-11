2b80e726bf8058104835d4bb275b14df7f4ed895
Piotr Kierzniewski <kierzniak@gmail.com>
2014-09-11 12:57:26 +0200

    Initial commit

---
+++ build/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# PHP Lint
+for i in $(find ./src -name '*.php'); do
+    php -l "$i"
+done
+
+# PHP Code Sniffer
+./vendor/bin/phpcs --standard=./build/phpcs.xml ./src
+
+# PHP Mess Detector
+./vendor/bin/phpmd ./src text ./build/phpmd.xml
+
+# PHP Copy/Paste Detecotr
+./vendor/bin/phpcpd ./src
+
+# PHPSpec
+# ./vendor/bin/phpspec run --format=pretty
---
+++ src/Harmony/Form.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Form class
+ *
+ * This file is part Harmony Form
+ *
+ * @category  Harmony
+ * @package   Form
+ * @author    ViewOne Sp. z o.o. <support@viewone.pl>
+ * @copyright 2014 ViewOne Sp. z o.o.
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @link      https://github.com/viewone
+ */
+
+namespace Harmony;
+
+class Form
+{
+}
